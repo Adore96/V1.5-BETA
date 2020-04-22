c52ce0586cdec5ccea58106ec11ac29f734bb2a6
Kalhara Perera <adorekasun@gmail.com>
2020-04-22 13:33:46 +0530

    Initial Commit

---
+++ CuRec/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CuRec
+//
+//  Created by Kalhara Perera on 4/22/20.
+//  Copyright © 2020 Kalhara Perera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
